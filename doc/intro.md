dd956902b01b660633a28a4938113041cdf03046
Merlin Webster <mjftwebster@gmail.com>
2021-02-12 08:47:04 +0000

    Generated new lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to simple-lang
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/simple_lang/core_test.clj
@@ -0,0 +1,7 @@
+(ns simple-lang.core-test
+  (:require [clojure.test :refer :all]
+            [simple-lang.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
