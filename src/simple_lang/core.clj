68ed6f3a849f9500b6d5004165dd01de91f699b6
Merlin Webster <mjftwebster@gmail.com>
2021-02-13 08:52:32 +0000

    Added assignment

--- src/simple_lang/core.clj
+++ src/simple_lang/core.clj
@@ -3,7 +3,7 @@
 (defprotocol Expression
   (reducible? [this])
   (->str [this])
-  (reduce-e [this]))
+  (reduce-e [this env]))
 
 (defn inspect [exp]
   (str "<<" (->str exp) ">>"))
@@ -12,32 +12,48 @@
   Expression
   (reducible? [this] false)
   (->str [this] (str (:value this)))
-  (reduce-e [this] (:value this)))
+  (reduce-e [this env] (:value this)))
 
 (defrecord Add-e [left right]
   Expression
   (reducible? [this] true)
   (->str [this] (str (->str (:left this)) " + " (->str (:right this))))
-  (reduce-e [this]
+  (reduce-e [this env]
     (let [left (:left this) right (:right this)]
       (cond
-        (reducible? left) (->Add-e (reduce-e left) right)
-        (reducible? right) (->Add-e left (reduce-e right))
-        :else (->Number-e (+ (reduce-e left) (reduce-e right)))))))
+        (reducible? left) (->Add-e (reduce-e left env) right)
+        (reducible? right) (->Add-e left (reduce-e right env))
+        :else (->Number-e (+ (reduce-e left env) (reduce-e right env)))))))
 
 (defrecord Mult-e [left right]
   Expression
   (reducible? [this] true)
   (->str [this] (str (->str (:left this)) " * " (->str (:right this))))
-  (reduce-e [this]
+  (reduce-e [this env]
     (let [left (:left this) right (:right this)]
       (cond
-        (reducible? left) (->Mult-e (reduce-e left) right)
-        (reducible? right) (->Mult-e left (reduce-e right))
-        :else (->Number-e (* (reduce-e left) (reduce-e right)))))))
+        (reducible? left) (->Mult-e (reduce-e left env) right)
+        (reducible? right) (->Mult-e left (reduce-e right env))
+        :else (->Number-e (* (reduce-e left env) (reduce-e right env)))))))
+
+(defrecord EnvChange-e [environment]
+  Expression
+  (reducible? [this] false)
+  (->str [this] (str "env-change: " (:environment this)))
+  (reduce-e [this env] this))
+
+(defrecord Assign-e [name, expression]
+  Expression
+  (reducible? [this] true)
+  (->str [this] (str (:name this) "=" (->str (:expression this))))
+  (reduce-e [this env]
+    (let [name (:name this) expression (:expression this)])
+    (if (reducible? expression)
+      (->Assign-e name (reduce-e expression env))
+      (->EnvChange-e (assoc env name expression)))))
 
 (defn run [expression]
   (println (inspect expression))
   (if (reducible? expression)
-    (recur (reduce-e expression))
+    (recur (reduce-e expression {}))
     expression))
--- test/simple_lang/core_test.clj
+++ test/simple_lang/core_test.clj
@@ -7,18 +7,29 @@
     (testing "is not reducible"
       (is (= false (reducible? exp))))
     (testing "reducing yields the value"
-      (is (= 5 (reduce-e exp))))))
+      (is (= 5 (reduce-e exp {}))))))
 
 (deftest Add-e
   (let [exp (->Add-e (->Number-e 7) (->Number-e 8))]
     (testing "is reducible"
       (is (= true (reducible? exp))))
     (testing "reducing yields correct number"
-      (is (= (->Number-e (+ 7 8)) (reduce-e exp))))))
+      (is (= (->Number-e (+ 7 8)) (reduce-e exp {}))))))
 
 (deftest Mult-e
   (let [exp (->Mult-e (->Number-e 10) (->Number-e 11))]
     (testing "is reducible"
       (is (= true (reducible? exp))))
     (testing "reducing yields correct number"
-      (is (= (->Number-e (* 10 11)) (reduce-e exp))))))
+      (is (= (->Number-e (* 10 11)) (reduce-e exp {}))))))
+
+(deftest run-machine
+  (testing "running gives expected result"
+    (let [exp (->Add-e
+               (->Add-e
+                (->Number-e 1)
+                (->Mult-e
+                 (->Number-e 2)
+                 (->Number-e 3)))
+               (->Number-e 4))]
+      (is (= (->Number-e 11) (run exp))))))
